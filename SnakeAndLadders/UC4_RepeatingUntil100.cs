8af0e16eb1a2d600dd2655f9b58f4ef61c757406
Sumeetacz <sumeetacz@gmail.com>
2022-04-16 18:48:03 +0530

    UC4 Repeating until 100 comes

---
+++ SnakeAndLadders/UC4_RepeatingUntil100.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    internal class UC4_RepeatingUntil100
+    {
+        class Simulation
+        {
+            public int Roll = 0;
+            public int Current_Place = 0;
+            public const int LADDER = 1;
+            public const int SNAKE = 2;
+            public const int NO_PLAY = 0;
+
+            public int Dice()
+            {
+                return new Random().Next(1, 7);
+            }
+
+            public int Check()
+            {
+                return new Random().Next(0, 3);
+            }
+
+            public void Play()
+            {
+                while (Current_Place < 100)
+                {
+                    int Roll = Dice();
+                    switch (Check())
+                    {
+                        case LADDER:
+                            Current_Place = Current_Place + Roll;
+                            break;
+                        case SNAKE:
+                            Current_Place = Current_Place - Roll;
+                            if (Current_Place <= 0)
+                            {
+                                Current_Place = 0;
+                            }
+                            break;
+                        case NO_PLAY:
+                            break;
+                    }
+                    Console.WriteLine("Current Position of player is: {0} \n", Current_Place);
+                }
+
+                Console.WriteLine("The player reached 100th position.");
+            }
+
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+                Console.WriteLine("Current Position of player is: 0 \n");
+                Simulation start = new Simulation();
+                start.Play();
+            }
+        }
+    }
+}
