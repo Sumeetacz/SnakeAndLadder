c941d93b5603f5d6aa09d0f50ae2a5c11ee4aa8a
Sumeetacz <sumeetacz@gmail.com>
2022-04-16 18:15:36 +0530

    UC1 snake and Ladder

---
+++ SnakeAndLadders/UC1_SnakeAndLadder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    class UC1_SnakeAndLadder
+    {
+       public static void Snake()
+        {
+            Console.WriteLine("Snake and Ladder game played with single player at start position 0");
+        }
+    }
+}
